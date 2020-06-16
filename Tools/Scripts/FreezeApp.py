6ecfad3eb37c50b82680d078e3df1cb7231e795d
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-06-16 14:33:15 +0200

    Fix FreezeApp script

--- Tools/Scripts/FreezeApp.py
+++ Tools/Scripts/FreezeApp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os, sys
-import easyAppGui, easyAppLogic
+import easyTemplateLib, easyAppGui, easyAppLogic
 from PyInstaller.__main__ import run
 
 current_path = os.path.dirname(sys.argv[0])
@@ -10,13 +10,14 @@ if sys.platform.startswith('win'):
     separator = ";"
 
 run([
-    os.path.join('easyTemplate', 'main.py'),
-    '--name=%s' % 'easyTemplate',
+    os.path.join('easyTemplateApp', 'main.py'),
+    '--name=%s' % 'easyTemplateApp',
     '--noconfirm',
     '--clean',
     '--windowed',
     '--onedir',
-    '--add-data={}{}.'.format('easyTemplate', separator),
+    '--add-data={}{}.'.format('easyTemplateApp', separator),
+    '--add-data={}{}.'.format(easyTemplateLib.__path__[0], separator),
     '--add-data={}{}.'.format(easyAppGui.__path__[0], separator),
     '--add-data={}{}.'.format(easyAppLogic.__path__[0], separator)
     #'--add-data=%s' % os.path.join('resource', 'path', '*.txt'),
